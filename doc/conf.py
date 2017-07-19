fb604b45c0f5f1ae3bbaa6150b32c408d7c85250
Jan Hermann <dev@hermann.in>
2017-07-19 10:11:28 +0200

    add doc bones

---
+++ doc/conf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode'
+]
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'pyberny'
+author = 'Jan Hermann'
+copyright = f'2017 {author}'
+version = '0.1'
+release = version
+language = None
+exclude_patterns = ['build', '.DS_Store']
+pygments_style = 'sphinx'
+todo_include_todos = True
+html_theme = 'alabaster'
+htmlhelp_basename = f'{project}doc'
---
+++ doc/index.rst
@@ -0,0 +1,18 @@
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+``pyberny`` — Molecular optimizer
+=================================
+
+API
+---
+
+.. automodule:: berny.berny
+   :members:
+
+.. automodule:: berny.geomlib
+   :members:
+
+.. automodule:: berny.species_data
+   :members:
