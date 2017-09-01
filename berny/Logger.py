a2a6731d9a3c0f12e0af383410b559eba044226d
Jan Hermann <dev@hermann.in>
2017-09-01 11:54:33 +0200

    add regex filtering to loggers

--- berny/Logger.py
+++ berny/Logger.py
@@ -2,16 +2,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import print_function
+import re
 
 
 class Logger(object):
-    def __init__(self, verbosity=0, out=None):
+    def __init__(self, verbosity=0, out=None, regex=None):
         self.verbosity = verbosity
         self.out = out
         self.n = 0
+        self.regex = regex
 
     def __call__(self, msg, **kwargs):
         level = kwargs.get('level', 0)
         if level < -self.verbosity:
+            return
+        if self.regex is not None and not re.search(self.regex, msg):
             return
         print(self.n, msg, file=self.out)
