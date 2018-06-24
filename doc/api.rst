42638b13d14a058c486e16871fbfc468bd39f9a0
Jan Hermann <dev@janhermann.cz>
2018-06-24 02:27:50 +0200

    update doc

---
+++ doc/api.rst
@@ -0,0 +1,12 @@
+API
+===
+
+.. module:: berny
+
+.. autofunction:: Berny
+
+.. automodule:: berny.geomlib
+   :members:
+
+.. automodule:: berny.species_data
+   :members:
--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -10,7 +11,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = 'pyberny'
 author = 'Jan Hermann'
-copyright = f'2017 {author}'
+copyright = f'2017-{datetime.date.today().year}, {author}'
 version = '0.1'
 release = version
 language = None
@@ -18,6 +19,20 @@ exclude_patterns = ['build', '.DS_Store']
 pygments_style = 'sphinx'
 todo_include_todos = True
 html_theme = 'alabaster'
+html_theme_options = {
+    'description': 'Molecular geometry optimizer',
+    'github_button': True,
+    'github_user': 'azag0',
+    'github_repo': 'pyberny',
+}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
 htmlhelp_basename = f'{project}doc'
 
 
